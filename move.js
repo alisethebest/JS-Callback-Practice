0c00e9ad9a70c6de9a25bd5a27ebcbf0c7df1371
Frank Cervantes <109487989+alisethebest@users.noreply.github.com>
2022-09-28 17:33:22 -0700

    Update move.js

--- move.js
+++ move.js
@@ -1,12 +1,40 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
-    }
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
 
-    return {
-        to: moveToCoordinates
-    }
+  return {
+    to: moveToCoordinates,
+  };
 }
+Footer;
+
+function moveWithArrowKeys(left, bottom, callback){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+   
+    document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    callback(direction)
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+    callback(direction)
